8e2e2a9266cc629d3a1bc647edd706c84196a658
hellowitsme <75776428+hellowitsme@users.noreply.github.com>
2021-04-01 14:30:55 +0900

    Initial Commit.

---
+++ function/enumerate.py
@@ -0,0 +1,7 @@
+movies = ["the shining", "john wick", "equalizer", "top gun"]
+for i, movie in enumerate(movies):
+    movie = movies[i]
+    movie = movie.upper()
+    movies[i] = movie
+
+print(movies)
